75d7e886286a41e8f03012b8f8e846bc8f4456db
Evandro Santos <vandersant7@gmail.com>
2024-05-05 20:12:32 -0300

    components dinamics propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
 export class SmallCardComponent {
-
+  @Input() photoCover: string = '';
+  @Input() cardTitle: string = '';
 }
